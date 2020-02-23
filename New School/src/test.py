6c04787aa98a7c5d1d1443227dd883eb8337f7c9
Samyouvi <35795390+Samyouvi@users.noreply.github.com>
2020-02-24 00:09:25 +0100

    update code + rapport

--- New School/src/test.py
+++ New School/src/test.py
@@ -17,23 +17,23 @@ model = load_model('CorelDB_model.h5')
 
 img_width, img_height = 64, 64
 
-train_dir = "../dataset/test"
-validation_dir = "../dataset/res"
+test_dir = "../dataset/test"
+res_dir = "../dataset/res"
 
-nb_test_samples = sum([len(files) for r, d, files in os.walk(train_dir)])
+nb_test_samples = sum([len(files) for r, d, files in os.walk(test_dir)])
 
 batch_holder = np.zeros((nb_test_samples, img_width, img_height, 3))
 y_true = np.zeros(nb_test_samples)
 
-class_names = next(os.walk(train_dir))[1]
+class_names = next(os.walk(test_dir))[1]
 class_names.sort()
 
 i = 0
-for dirpath, dirnames, filenames in os.walk(train_dir):
+for dirpath, dirnames, filenames in os.walk(test_dir):
     for imgnm in filenames:
         img = image.load_img(os.path.join(dirpath, imgnm), target_size=(img_width, img_height))
         batch_holder[i, :] = img
-        y_true[i] = int(class_names.index(os.path.relpath(dirpath, train_dir)))
+        y_true[i] = int(class_names.index(os.path.relpath(dirpath, test_dir)))
         i = i + 1
 
 y_pred = model.predict_classes(batch_holder)
@@ -43,16 +43,16 @@ classification, confusion = reports(y_pred, y_true, class_names)
 print(classification)
 print(confusion)
 
-for dirpath, dirnames, filenames in os.walk(train_dir):
-    structure = os.path.join(validation_dir, os.path.relpath(dirpath, train_dir))
+for dirpath, dirnames, filenames in os.walk(test_dir):
+    structure = os.path.join(res_dir, os.path.relpath(dirpath, test_dir))
     if not os.path.isdir(structure):
         os.mkdir(structure)
     else:
         print("Folder already exists")
 
 i = 0
-for dirpath, dirnames, filenames in os.walk(train_dir):
-    structure = os.path.join(validation_dir, os.path.relpath(dirpath, train_dir))
+for dirpath, dirnames, filenames in os.walk(test_dir):
+    structure = os.path.join(res_dir, os.path.relpath(dirpath, test_dir))
     for imgnm in filenames:
-        shutil.copy(dirpath + '/' + imgnm, validation_dir + '/' + class_names[y_pred[i]] + '/' + imgnm)
+        shutil.copy(dirpath + '/' + imgnm, res_dir + '/' + class_names[y_pred[i]] + '/' + imgnm)
         i = i + 1
